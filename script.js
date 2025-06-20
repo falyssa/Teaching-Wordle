0992e467393f1fca36375f16e8791d32917863c3
isuh <115388269+falisuh@users.noreply.github.com>
2025-06-19 22:35:44 -0400

    ...erm idk

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ let nextLetter = 0;
 let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
 
 console.log(rightGuessString);
+
+function initBoard() {
+    let board = document.getElementById("game-board");
+
+    // Loop that creates the rows and boxes based on the guesses / word length
+    for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
+        let row = document.createElement("div")
+        row.className = "letter-row"
+
+        for (let j = 0; j < 5; j++) {
+            let box = document.createElement("div")
+            box.className = "letter-box"
+            row.appendChild(box)
+        }
+        board.appendChild(row)
+    }
+}
+
+initBoard()
+
+// Creates one row for each guess we give the user and creates 5 boxes for each row. 
+// There is one box for each letter of the guess, and the function makes them all children of the row.
