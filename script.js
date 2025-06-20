c36f774d9bacc73f40e5dbceb2d3b94778bfd106
isuh <115388269+falisuh@users.noreply.github.com>
2025-06-19 22:16:06 -0400

    First few steps of the tutorial

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const GUESSES = 6;
 let guessesRemaining = GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
+let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)]
+
+console.log(rightGuessString);
