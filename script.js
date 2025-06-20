8fa757faf04bf54ad5b5a184f7fa2c7c975a39ac
isuh <115388269+falisuh@users.noreply.github.com>
2025-06-20 00:17:32 -0400

    Wait its working now

--- script.js
+++ script.js
@@ -29,3 +29,52 @@ initBoard()
 
 // Creates one row for each guess we give the user and creates 5 boxes for each row. 
 // There is one box for each letter of the guess, and the function makes them all children of the row.
+
+// Input code
+document.addEventListener("keyup", (e) => {
+    if (guessesRemaining === 0) {
+        return
+    }
+
+    let pressedKey = String(e.Key) 
+    if (pressedKey === "Backspace" && nextLetter !== 0) {
+        deleteLetter()
+        return
+    }
+
+    if (pressedKey === "Enter") {
+        checkGuess()
+        return
+    }
+
+    let found = pressedKey.match(/[a-z]/gi) // Checks if the input is a letter or not
+    if (!found || found.length > 1) {
+        return
+    } else {
+        insertLetter(pressedKey)
+    }
+})
+
+// Input functions
+function insertLetter (pressedKey) {
+    if (nextLetter === 5) {
+        return
+    }
+    pressedKey = pressedKey.toLowerCase()
+
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+    let box = row.children[nextLetter]
+    box.textContent = pressedKey
+    box.classList.add("filled-box")
+    currentGuess.push(pressedKey)
+    nextLetter += 1
+}
+
+function deleteLetter () {
+    let row = document.getElementsByClassName("letter-row")[6 - guessesRemaining]
+    let box = row.children[nextLetter - 1]
+    box.textContent = ""
+    box.classList.remove("filled-box")
+    currentGuess.pop()
+    nextLetter -= 1
+}
